0f8a7896967a1fe866760cf82acf8c5dd8e5a09e
Md Repon <repon7253@gmail.com>
2025-02-14 08:01:15 +0600

    feat: add console log for userData in App component during login

--- MegaBlog(appwrite backend as a service)/src/App.jsx
+++ MegaBlog(appwrite backend as a service)/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   useEffect(()=>{
     authService.getCurrentUser()
     .then((userData) => {
+      console.log('userData when login', userData)
       if (userData) {
         dispatch(login({userData}))
       } else {
