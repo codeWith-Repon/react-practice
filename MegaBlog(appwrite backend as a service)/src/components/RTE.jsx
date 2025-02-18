5c1fafafaa36fd9000da64de87a63871aec7af67
Md Repon <repon7253@gmail.com>
2025-02-18 15:13:12 +0500

    feat: refactor RTE component to default export and implement routing in main.jsx

--- MegaBlog(appwrite backend as a service)/src/components/RTE.jsx
+++ MegaBlog(appwrite backend as a service)/src/components/RTE.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
 
-export const RTE = ({name, control, label, defaultValue=""}) => {
+const RTE = ({name, control, label, defaultValue=""}) => {
   return (
     <div className='w-full'>
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -50,3 +50,5 @@ export const RTE = ({name, control, label, defaultValue=""}) => {
     </div>
   )
 }
+
+export default RTE;
